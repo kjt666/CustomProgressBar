8ccb8cbce89bbef4adaf18ed65e43eb106d31647
kjt666 <kjt333@126.com>
2016-07-01 17:32:49 +0800

    学了些新知识，发现跟好的实现方法，大幅度优化代码。结构更加清晰，条理更加顺畅。

--- src/main/java/com/xinzhen/customprogressbar2/customview/MyHoriztalProgressBar2.java
+++ src/main/java/com/xinzhen/customprogressbar2/customview/MyHoriztalProgressBar2.java
@@ -7,33 +7,31 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.widget.ProgressBar;
 
 import com.xinzhen.customprogressbar2.R;
 
+
 /**
- * Created by C058 on 2016/6/14.
+ * Created by C058 on 2016/7/1.
  */
 public class MyHoriztalProgressBar2 extends ProgressBar {
 
-    private static final int DEFAULT_PROGRESSBAR_HEIGHT = 10;
-    private static final int DEFAULT_REACH_COLOR = 0xfff456c5;
-    private static final int DEFAULT_UNREACH_COLOR = 0xFFCECECE;
-    private static final int DEFAULT_PROGRESSBAR_RADIUS = DEFAULT_PROGRESSBAR_HEIGHT / 2;
+    private final int PROGRESSBAR_DEFAULT_HEIGHT = dp2px(10);//px
+    private final int PROGRESSBAR_DEFAULT_REACH_COLOR = 0xfff456c5;
+    private final int PROGRESSBAR_DEFAULT_UNREACH_COLOR = 0xFFCECECE;
 
-    private int mProgressbarHeight = dp2px(DEFAULT_PROGRESSBAR_HEIGHT);
-    private int mReachColor = DEFAULT_REACH_COLOR;
-    private int mUnReachColor = DEFAULT_UNREACH_COLOR;
-    private int mProgressbarRadius = dp2px(DEFAULT_PROGRESSBAR_RADIUS);
-
-    private int mRealWidth, mRealHeight;
+    private int mProgressbarHeight;
+    private int mReachColor;
+    private int mUnReachColor;
+    private int mRealWidth;
     private Paint mPaint;
-    private RectF mRectf;
 
     private Bitmap mDstBmp, mSrcBmp;
+    private Canvas mSrcCanvas;
+    float progressX;
 
     public MyHoriztalProgressBar2(Context context) {
         this(context, null);
@@ -45,10 +43,13 @@ public class MyHoriztalProgressBar2 extends ProgressBar {
 
     public MyHoriztalProgressBar2(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.MyHoriztalProgressBar2);
-        mReachColor = ta.getColor(R.styleable.MyHoriztalProgressBar2_reach_color, mReachColor);
-        mUnReachColor = ta.getColor(R.styleable.MyHoriztalProgressBar2_unreach_color, mUnReachColor);
-        mPaint = new Paint();
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.MyHoriztalProgressBar2);
+        mProgressbarHeight = (int) ta.getDimension(R.styleable.MyHoriztalProgressBar2_progressbar_height, PROGRESSBAR_DEFAULT_HEIGHT);
+        mReachColor = ta.getColor(R.styleable.MyHoriztalProgressBar2_reach_color, PROGRESSBAR_DEFAULT_REACH_COLOR);
+        mUnReachColor = ta.getColor(R.styleable.MyHoriztalProgressBar2_unreach_color, PROGRESSBAR_DEFAULT_UNREACH_COLOR);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStrokeWidth(mProgressbarHeight);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 
     @Override
@@ -58,80 +59,83 @@ public class MyHoriztalProgressBar2 extends ProgressBar {
 
         setMeasuredDimension(widthVal, heightVal);
         mRealWidth = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
-        mRealHeight = getMeasuredHeight() - getPaddingTop() - getPaddingBottom();
-        mProgressbarHeight = mRealHeight;
-        mProgressbarRadius = mProgressbarHeight / 2;
-        mRectf = new RectF(0, 0, mProgressbarRadius * 2, mProgressbarRadius * 2);
-        mDstBmp = getBackGround();
+        mDstBmp = getDstPic();//完成测量后再创建图片\
+        mSrcBmp = Bitmap.createBitmap(getMeasuredWidth(), mProgressbarHeight, Bitmap.Config.ARGB_8888);
     }
 
     @Override
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        float radio = getProgress() * 1.0f / getMax();
-        float finshedX = mRealWidth * radio;
-        int layerID1 = canvas.saveLayer(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint, Canvas.ALL_SAVE_FLAG);
-        canvas.translate(getPaddingLeft(), getPaddingTop());
+
+        progressX = mRealWidth * getProgress() / 100;
+        mSrcBmp = getSrcPic(progressX);
+        int layer = canvas.saveLayer(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint, Canvas.ALL_SAVE_FLAG);
+        canvas.translate(getPaddingLeft(), (getMeasuredHeight() - mProgressbarHeight) / 2); //这个高度上的位移让我想了很久。画图和普通的画线不一样。画图是从图片的（0,0）点开始画的，画线是从（0，线高度的一半）开始画的。
         canvas.drawBitmap(mDstBmp, 0, 0, mPaint);
-//        canvas.drawLine(0,0,mRealWidth,0,mPaint);
-        if (finshedX > 0) {
-            mSrcBmp = getProgressX(finshedX);
-            mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-            canvas.drawBitmap(mSrcBmp, 0, 0, mPaint);
-            mPaint.setXfermode(null);
-        }
-        canvas.restoreToCount(layerID1);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        canvas.drawBitmap(mSrcBmp, 0, 0, mPaint);
+        mPaint.setXfermode(null);
+        canvas.restoreToCount(layer);
     }
 
+    /**
+     * 测量控件高度
+     *
+     * @param heightMeasureSpec
+     * @return
+     */
     private int measureHeight(int heightMeasureSpec) {
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int mode = MeasureSpec.getMode(heightMeasureSpec);
-        int miniHeight = getPaddingTop() + getPaddingBottom() + mProgressbarHeight;
         int result = 0;
         if (mode == MeasureSpec.EXACTLY) {
-            result = height < miniHeight ? miniHeight : height;
-        } else if (mode == MeasureSpec.UNSPECIFIED || mode == MeasureSpec.AT_MOST) {
-            result = miniHeight;
-//            if (mode == MeasureSpec.AT_MOzST) {
-//                result = Math.min(result, height);
-//            }
+            result = height;
+        } else if (mode == MeasureSpec.AT_MOST) {
+            result = getPaddingTop() + getPaddingBottom() + mProgressbarHeight * 2;
         }
         return result;
     }
 
+    /**
+     * 获取源图像，因为使用次数较多，所以将一些资源抽了出来，避免频繁gc操作，耗费内存
+     *
+     * @param progress
+     * @return
+     */
+    private Bitmap getSrcPic(float progress) {
+        mSrcCanvas = new Canvas(mSrcBmp);
+        mPaint.setColor(mReachColor);
+        //canvas.drawLine(mProgressbarHeight, mProgressbarHeight / 2, progress - mProgressbarHeight, mProgressbarHeight / 2, paint);
+        if (getProgress() <= 50) {
+            mSrcCanvas.drawLine(0, mProgressbarHeight / 2, progress, mProgressbarHeight / 2, mPaint);
+        } else {
+            mSrcCanvas.drawLine(0, mProgressbarHeight / 2, progress - mProgressbarHeight / 2, mProgressbarHeight / 2, mPaint);
+        }
+        return mSrcBmp;
+    }
+
+    /**
+     * 获取目标图层图像，初始一次就行了。用临时变量
+     *
+     * @return
+     */
+    private Bitmap getDstPic() {
+        //创建画布，不用mRealWidth的原因是加入给控件设定10dp宽度，在设定10dp的pading，那么这个mReaalWidth则为负数，创建bitmap会报错，同理，高度我们也是限定了的
+        Bitmap bitmap = Bitmap.createBitmap(getMeasuredWidth(), mProgressbarHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        mPaint.setColor(mUnReachColor);
+        //因为线帽是线段两头的样式，是多出来的一部分，如果不设置线帽从0开始就可以，线帽长度一半为线的高度。
+        canvas.drawLine(mProgressbarHeight, mProgressbarHeight / 2, mRealWidth - mProgressbarHeight, mProgressbarHeight / 2, mPaint);
+        return bitmap;
+    }
+
+    /**
+     * dp转px
+     *
+     * @param dp
+     * @return
+     */
     private int dp2px(int dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
-    }
-
-    private Bitmap getBackGround() {
-        //创建画布，不用mRealWidth的原因是加入给控件设定10dp宽度，在设定10dp的pading，那么这个mReaalWidth则为负数，创建bitmap会报错，同理，高度我们也是限定了的
-//        Bitmap bitmap = Bitmap.createBitmap(mRealWidth + mProgressbarRadius, mRealHeight, Bitmap.Config.ARGB_8888);
-        Bitmap bitmap = Bitmap.createBitmap(getMeasuredWidth() + mProgressbarRadius, getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(mUnReachColor);
-        paint.setStrokeWidth(2 * mProgressbarHeight);
-        //开头的半圆
-        canvas.drawArc(mRectf, 90, 180, false, paint);
-        //直线
-        canvas.drawLine(mProgressbarRadius - mProgressbarRadius / 10, 0, mRealWidth, 0, paint);
-        //结尾的半圆
-        canvas.translate(mRealWidth - mProgressbarRadius, 0);
-        canvas.drawCircle(mProgressbarRadius, mProgressbarRadius, mProgressbarRadius, paint);
-        return bitmap;
-    }
-
-    private Bitmap getProgressX(float progress) {
-        //加上半径主要是为了能够显示出已完成进度条的最后那半个圆
-        Bitmap bitmap = Bitmap.createBitmap((int) progress + (mProgressbarRadius), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        canvas.drawColor(mUnReachColor);
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(mReachColor);
-        paint.setStrokeWidth(2 * mProgressbarHeight);
-        canvas.drawLine(0, 0, progress, 0, paint);
-        canvas.translate(progress - mProgressbarRadius, 0);
-        canvas.drawCircle(mProgressbarRadius, mProgressbarRadius, mProgressbarRadius, paint);
-        return bitmap;
     }
 }
